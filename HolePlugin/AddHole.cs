42f19b2297c83b2d24f36f2ac2277d6a46540ea9
Ксения Финаева <polya.makova@outlook.com>
2022-04-05 23:50:25 +0300

    Task 8

--- HolePlugin/AddHole.cs
+++ HolePlugin/AddHole.cs
@@ -1,6 +1,7 @@
 ﻿using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.DB.Mechanical;
+using Autodesk.Revit.DB.Plumbing;
 using Autodesk.Revit.DB.Structure;
 using Autodesk.Revit.UI;
 using System;
@@ -18,119 +19,212 @@ namespace HolePlugin
         {
             // основной документ-файл
             Document arDoc = commandData.Application.ActiveUIDocument.Document;
-            // связанный документ-файл
             Document ovDoc = arDoc.Application.Documents.OfType<Document>().Where(x => x.Title.Contains("ОВ")).FirstOrDefault();
-            //если вдруг файл не будет обнаружен, то
+
             if (ovDoc == null)
             {
                 TaskDialog.Show("Ошибка", "Не найден файл ОВ");
                 return Result.Cancelled;
             }
 
-            // убедимся, что в файл АР загружено семейство с отверстиями
-            FamilySymbol familySymbol = new FilteredElementCollector(arDoc)
-                .OfClass(typeof(FamilySymbol))
-                .OfCategory(BuiltInCategory.OST_GenericModel)
-                .OfType<FamilySymbol>()
-                .Where(x => x.Name.Equals("Тип 1"))
-                .FirstOrDefault();
+            List<Duct> ducts = GetDucts(ovDoc);
+            List<Pipe> pipes = GetPipes(ovDoc);
+            View3D view3D = GetView3D(arDoc);
+            FamilySymbol familySymbolRectangleHole = GetFamilySymbol1(arDoc);
+            FamilySymbol familySymbolRoundHole = GetFamilySymbol2(arDoc);
 
             // в случае, когда семейство не найдено
-            if (familySymbol == null)
+            if (familySymbolRectangleHole == null &&
+                familySymbolRoundHole == null)
             {
-                TaskDialog.Show("Ошибка", "Не найдено данное семейство \"Отверстия\"");
+                TaskDialog.Show("Ошибка", $"Не найдено данное семейство:{Environment.NewLine}\"Отверстие\"{Environment.NewLine}\"Круглое отверстие\""); ;
                 return Result.Cancelled;
             }
 
+            try
+            {
+                CreateRectangleHole(arDoc, ducts, view3D, familySymbolRectangleHole);
+                CreateRoundHole(arDoc, pipes, view3D, familySymbolRoundHole);
+            }
+            catch (OperationCanceledException)
+            {
+                return Result.Cancelled;
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return Result.Cancelled;
+            }
+
+            return Result.Succeeded;
+        }
+
+        private static List<Pipe> GetPipes(Document _ovDoc)
+        {
+            // найдём все трубы
+            List<Pipe> pipes = new FilteredElementCollector(_ovDoc)
+                .OfClass(typeof(Pipe))
+                .OfType<Pipe>()
+                .ToList();
+
+            return pipes;
+        }
+
+        private static List<Duct> GetDucts(Document ovDoc)
+        {
             // найдём все воздуховоды
             List<Duct> ducts = new FilteredElementCollector(ovDoc)
                 .OfClass(typeof(Duct))
                 .OfType<Duct>()
                 .ToList();
 
+            return ducts;
+        }
+
+        private static View3D GetView3D(Document arDoc)
+        {
             // найдём 3D вид
             View3D view3D = new FilteredElementCollector(arDoc)
                 .OfClass(typeof(View3D))
                 .OfType<View3D>()
-                // важно, чтобы 3D вид не являлся шаблоном вида, поэтому доавим усл-е, что св-во IsTemplate не установлено
                 .Where(x => !x.IsTemplate)
                 .FirstOrDefault();
 
-            // в случае, когда 3D вид не найден
             if (view3D == null)
             {
                 TaskDialog.Show("Ошибка", "Не найден 3D вид");
-                return Result.Cancelled;
+                return null;
             }
 
-            // создаём объект ReferenceIntersector
+            return view3D;
+        }
+
+        private static FamilySymbol GetFamilySymbol1(Document arDoc)
+        {
+            FamilySymbol familySymbolRectangleHole = new FilteredElementCollector(arDoc)
+                .OfClass(typeof(FamilySymbol))
+                .OfCategory(BuiltInCategory.OST_GenericModel)
+                .OfType<FamilySymbol>()
+                .Where(x => x.FamilyName.Equals("Отверстие"))
+                .FirstOrDefault();
+
+            return familySymbolRectangleHole;
+        }
+
+        private static FamilySymbol GetFamilySymbol2(Document arDoc)
+        {
+            // убедимся, что в файл АР загружено семейство с отверстиями
+            FamilySymbol familySymbolRoundHole = new FilteredElementCollector(arDoc)
+                .OfClass(typeof(FamilySymbol))
+                .OfCategory(BuiltInCategory.OST_GenericModel)
+                .OfType<FamilySymbol>()
+                .Where(x => x.FamilyName.Equals("Круглое отверстие"))
+                .FirstOrDefault();
+
+            return familySymbolRoundHole;
+        }
+
+        private static void CreateRectangleHole(Document arDoc, List<Duct> ducts, View3D view3D, FamilySymbol familySymbolRectangleHole)
+        {
             ReferenceIntersector referenceIntersector = new ReferenceIntersector(
-                // мы хотим фильтровать по классу
                 new ElementClassFilter(typeof(Wall)),
-                // мы хотим найти элемент
                 FindReferenceTarget.Element,
                 view3D);
 
+            Transaction ts = new Transaction(arDoc);
+            ts.Start("Активация отверстий");
+            if (!familySymbolRectangleHole.IsActive)
+            {
+                familySymbolRectangleHole.Activate();
+            }
+            ts.Commit();
+
             Transaction tr = new Transaction(arDoc);
-            tr.Start("Расстановка отверстий");
+            tr.Start("Расстановка прямоугольных отверстий");
 
-            if (!familySymbol.IsActive)
-                familySymbol.Activate();
-
-            // чтобы получить результат, т.е. набор пересекаемых стен. А применять будет с каждым воздуховодом, найденным в коллекции ducts
             foreach (Duct duct in ducts)
             {
                 Line curve = (duct.Location as LocationCurve).Curve as Line;
-
-                // теперь точку, можно получить из curve
                 XYZ point = curve.GetEndPoint(0);
-
-                // вектор направления
                 XYZ direction = curve.Direction;
 
-                // методу Find нужно передать 2 аргумента: исходная точка и направление из этой точки, а точку - из кривой
                 List<ReferenceWithContext> intersections = referenceIntersector.Find(point, direction)
-                    // ограничим нашу коллекцию. нужны объекты, у кот-х Proximity(расстояние) не превышает длину воздуховода
                     .Where(x => x.Proximity <= curve.Length)
-                    // добавляем метод расширения Distinct - из всех объектов, кот. совпадают по зад. критерию, оставляет только один. в кач-ве аргумента указ-м экземпляр класса ReferenceWithContextElementEqualityComparer
                     .Distinct(new ReferenceWithContextElementEqualityComparer())
                     .ToList();
 
-                // в итоге получим набор всех пересечений в виде объектов ReferenceWithContext
                 foreach (ReferenceWithContext rwc in intersections)
                 {
-                    // определим точку вставки и добавим туда экземпляр семейства "Отверстия"
                     double proximity = rwc.Proximity;
                     Reference reference = rwc.GetReference();
-
-                    // зная ElementId можно получить в док-те непосредственно стену, на кот-ю идёт эта сссылка
-                    Wall wall = arDoc.GetElement(reference.ElementId) as Wall; // на выходе получим элемент
-                    // получим уровень из стены
+                    Wall wall = arDoc.GetElement(reference.ElementId) as Wall;
                     Level level = arDoc.GetElement(wall.LevelId) as Level;
-
                     XYZ pointHole = point + (direction * proximity);
 
-                    // вставка экземпляра семейства
-                    FamilyInstance holes = arDoc.Create.NewFamilyInstance(pointHole, familySymbol, wall, level, StructuralType.NonStructural);
-
-                    // нам нужны длина и ширина, определим из диаметра трубы
+                    FamilyInstance holes = arDoc.Create.NewFamilyInstance(pointHole, familySymbolRectangleHole, wall, level, StructuralType.NonStructural);
                     Parameter width = holes.LookupParameter("Ширина");
                     Parameter height = holes.LookupParameter("Высота");
 
-                    // установим значение Set, кот-й возьмём у воздуховода
                     width.Set(duct.Diameter);
                     height.Set(duct.Diameter);
                 }
             }
+
+            tr.Commit();
+        }
+
+        private static void CreateRoundHole(Document arDoc, List<Pipe> pipes, View3D view3D, FamilySymbol familySymbolRoundHole)
+        {
+            ReferenceIntersector referenceIntersector = new ReferenceIntersector(
+                new ElementClassFilter(typeof(Wall)),
+                FindReferenceTarget.Element,
+                view3D);
+
+            Transaction tr = new Transaction(arDoc);
+            tr.Start("Активация круглых отверстий");
+            if (!familySymbolRoundHole.IsActive)
+            {
+                familySymbolRoundHole.Activate();
+            }
+
             tr.Commit();
 
-            return Result.Succeeded;
+            Transaction ts = new Transaction(arDoc);
+            ts.Start("Расстановка круглых отверстий");
+
+            foreach (Pipe pipe in pipes)
+            {
+                Line curve = (pipe.Location as LocationCurve).Curve as Line;
+                XYZ point = curve.GetEndPoint(0);
+                XYZ direction = curve.Direction;
+
+                List<ReferenceWithContext> intersections = referenceIntersector.Find(point, direction)
+                    .Where(x => x.Proximity <= curve.Length)
+                    .Distinct(new ReferenceWithContextElementEqualityComparer())
+                    .ToList();
+
+                foreach (ReferenceWithContext rwc in intersections)
+                {
+                    double proximity = rwc.Proximity;
+                    Reference reference = rwc.GetReference();
+                    Wall wall = arDoc.GetElement(reference.ElementId) as Wall;
+                    Level level = arDoc.GetElement(wall.LevelId) as Level;
+                    XYZ pointHole = point + (direction * proximity);
+
+                    FamilyInstance holes = arDoc.Create.NewFamilyInstance(pointHole, familySymbolRoundHole, wall, level, StructuralType.NonStructural);
+                    Parameter rad = holes.LookupParameter("Радиус");
+                    double diam = pipe.get_Parameter(BuiltInParameter.RBS_PIPE_OUTER_DIAMETER).AsDouble();
+
+                    rad.Set(diam / 2);
+                }
+            }
+
+            ts.Commit();
         }
     }
 
     public class ReferenceWithContextElementEqualityComparer : IEqualityComparer<ReferenceWithContext>
     {
-        // определяет, убдут ли два заданных объекта (аргументы) определяемыми
         public bool Equals(ReferenceWithContext x, ReferenceWithContext y)
         {
             if (ReferenceEquals(x, y)) return true;
@@ -138,16 +232,12 @@ namespace HolePlugin
             if (ReferenceEquals(null, y)) return false;
 
             var xReference = x.GetReference();
-
             var yReference = y.GetReference();
 
-            // если у обоих элементов одинаковые ElementId, то получим точки на одной стене, то вернётся true
-            // LinkedElementId - ElementId из связанного файла, они так же д.б. одинаковы
             return xReference.LinkedElementId == yReference.LinkedElementId
                        && xReference.ElementId == yReference.ElementId;
         }
 
-        // должен возвращать HashCode объекта
         public int GetHashCode(ReferenceWithContext obj)
         {
             var reference = obj.GetReference();
